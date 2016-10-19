91a3cd9fa3b489f94d8e8c8ace5e2da18b327be8
adventurebear <suzanneatkinson@gmail.com>
2016-10-18 21:19:32 -0400

    initial commit API templates

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by suzanne on 10/18/16.
+ */
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
